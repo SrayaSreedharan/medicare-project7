5b1746cdd961a706790f6a59d72857ee415fc8a8
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-12 13:26:52 +0530

    test

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ufzukjvpjpbqugbxtteh.supabase.co'; // ✅ Should match your Supabase project
+const supabaseUrl = 'https://ufzukjvpjpbqugbxtteh.supabase.co'; 
 
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmenVranZwanBicXVnYnh0dGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNDExMzIsImV4cCI6MjA2NzgxNzEzMn0.brDJ4EcBFgIpZuhb561up2s1m9JPMLju2o6kWapyIJM';
 
